382de93dffa9b8becad27a096cb8b3d1bb9a9697
Antony Jones <antony@beyonk.com>
2021-02-08 11:05:58 +0000

    remove reading token from package.json

--- config.js
+++ config.js
@@ -1,12 +1,7 @@
 'use strict'
 
 const minimist = require('minimist')
-let { token } = minimist(process.argv.slice(2))
-
-if (!token) {
-  const config = require('./package.json')
-  token = config['pino-sematext'].token
-}
+const { token } = minimist(process.argv.slice(2))
 
 if (!token) {
   throw new Error('Missing API Token. Use --token or `pino-sematext.token` in your package.json')
