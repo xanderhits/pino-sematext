0fb689fcd0ce122c0311d217fe8a64b87b951333
Xander Horjus <xander@horjus-it.nl>
2022-03-03 21:55:00 +0100

    feat: point to EU sematext

--- logger.js
+++ logger.js
@@ -2,6 +2,6 @@
 
 const Logsene = require('logsene-js')
 const { token } = require('./config.js')
-const logger = new Logsene(token)
+const logger = new Logsene(token, null, 'https://logsene-receiver.eu.sematext.com/_bulk')
 
 module.exports = logger
